b6996d95f808fb2d616dfffe9e055cc7bf6cabbc
pedr <pedrlz.frn@gmail.com>
2025-02-24 18:12:40 -0300

    improve previsibility of model

--- src/models/minicpm.ts
+++ src/models/minicpm.ts
@@ -21,11 +21,12 @@ export default class MiniCPM {
         const pathToLlamaCli = `${base}/build/bin/llama-minicpmv-cli`;
         const pathToModel = `${base}/../MiniCPM-o-2_6/model/MiniCPM-o-7.6B-2_6-F16.gguf`;
         const pathToModel2 = `${base}/../MiniCPM-o-2_6/mmproj-model-f16.gguf`;
-        const instruction = "Transcribe the content of the image into text. Output only the content. Be concise."
-        const command = `${pathToLlamaCli} -m ${pathToModel} --mmproj ${pathToModel2} -c 4096 --temp 0.7 --top-p 0.8 --top-k 100 --repeat-penalty 1.05 --image ${imagePath} -p "${instruction}"`
+        const instruction = "SYSTEM: you are an agent of a OCR system. Your job is to be concise and correct. You should NEVER deviate from the content of the image. You should NEVER add any context or new information. Your only job should be to transcribe the text presented in the image as text without anything new inforation. Use the first image as an example, the output for it should be: J'étudie français. Your turn:";
+        const pathToExampleImage = `${base}/../images/sample_1.png`;
+        const command = `${pathToLlamaCli} -m ${pathToModel} --mmproj ${pathToModel2} -c 4096 --temp 0.1 --top-p 0.8 --top-k 100 --repeat-penalty 1.05 --image ${pathToExampleImage} --image ${imagePath} -p "${instruction}"`
+
         const result = await exec(command);
-        console.error(result.stderr);
-        const [_, meaningfulOutput] = result.stdout.split('<assistant>');
-        return meaningfulOutput;
+        const log = result.stdout.split('<assistant>');
+        return (log[log.length -1]).trim();
     }
 }
